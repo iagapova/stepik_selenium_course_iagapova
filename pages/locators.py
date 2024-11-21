ff3eb493c3ba9494faac6fee59ca3c4d32b0de57
iagapova <iagapova@gmail.com>
2024-11-21 12:28:41 +0100

    login_page base object is added

---
+++ pages/locators.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    # LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    LOGIN_URL = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_link")
